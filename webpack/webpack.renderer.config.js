1a2bcfa7bbb1a638695135305a2e6aeff8421f0a
Jan-Niklas Voß <jan-niklas.voss@haw-hamburg.de>
2021-07-21 15:21:43 +0200

    refactor: add import aliases

--- webpack/webpack.renderer.config.js
+++ webpack/webpack.renderer.config.js
@@ -1,5 +1,7 @@
 const rules = require('./webpack.rules');
 const plugins = require('./webpack.plugins');
+const resolves = require('./webpack.resolves');
+
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 rules.push({
@@ -16,15 +18,15 @@ plugins.push(new MonacoWebpackPlugin({
     publicPath: ".webpack/renderer"
 }))
 
+resolves.alias = {
+    'vscode': require.resolve('monaco-languageclient/lib/vscode-compatibility')
+};
+resolves.extensions.push('.css', '.json', '.ttf');
+
 module.exports = {
     module: {
         rules,
     },
     plugins: plugins,
-    resolve: {
-        alias: {
-            'vscode': require.resolve('monaco-languageclient/lib/vscode-compatibility')
-        },
-        extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json', '.ttf']
-    },
+    resolve: resolves,
 };
