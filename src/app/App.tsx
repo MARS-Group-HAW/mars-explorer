75436b776f11d798f2a7143f9c5ea4310d604d73
Jan-Niklas Voß <github@jnvoss.de>
2021-08-04 11:16:27 +0200

    feat: handle .NET not found error

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,20 @@
 import * as React from "react";
-import { Component } from "react";
-import { Modeler } from "./Modeler";
+import {Component} from "react";
+import {Modeler} from "./Modeler";
+import {Channel} from "@shared/types/Channel";
 
 export class App extends Component<any, any> {
-  componentDidMount() {
-    window.api.logger.info("App mounted.");
-  }
+    componentDidMount() {
+        window.api.logger.info("App mounted.");
+        window.api.on(Channel.DOTNET_NOT_FOUND, this.onDotnetNotFound);
+    }
 
-  render() {
-    return <Modeler />;
-  }
+    onDotnetNotFound = () => {
+        window.api.logger.warn('.NET SDK not found.');
+        alert(`Installation of the .NET SDK was not found in your Path. Make sure to have it installed and available in your path. If you've just installed it, try restarting your machine.`)
+    }
+
+    render() {
+        return <Modeler/>;
+    }
 }
