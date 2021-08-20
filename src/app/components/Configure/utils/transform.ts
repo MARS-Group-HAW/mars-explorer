a8c50eee32b48c2999a4e9a5ac4737aeeda441e0
Jan-Niklas Voß <jan-niklas.voss@haw-hamburg.de>
2021-08-20 16:58:00 +0200

    feat: add transformer config -> form

--- src/app/components/Configure/utils/transform.ts
+++ src/app/components/Configure/utils/transform.ts
@@ -1,19 +1,65 @@
 import { TypeOf } from "yup";
+import TimeSpecification from "@app/components/Configure/components/globals-form/utils/types";
+import { OutputsValidationSchema } from "@app/components/Configure/components/outputs-form/utils/validationSchema";
+import OutputSpecification from "@app/components/Configure/components/outputs-form/utils/types";
 import validationSchema from "./validationSchema";
-
-import GlobalsFieldNames from "../components/globals-form/utils/fieldNames";
-import GlobalsDefaultValues from "../components/globals-form/utils/defaultValues";
+import defaultValues from "./defaultValues";
 import { GlobalsValidationSchema } from "../components/globals-form/utils/validationSchema";
 
 type FormSchema = TypeOf<typeof validationSchema>;
 
 class FormTransformer {
   public static configToForm(config: unknown): FormSchema {
-    const formSchema = validationSchema.cast(config);
-    (formSchema.globals as GlobalsValidationSchema)[
-      GlobalsFieldNames.TIME_SPECIFICATION
-    ] = GlobalsDefaultValues[GlobalsFieldNames.TIME_SPECIFICATION];
-    return formSchema;
+    let formSchema = validationSchema.cast(config);
+    formSchema = FormTransformer.removeEmptyObj(
+      FormTransformer.removeUndefined(formSchema)
+    );
+
+    const parsedConfig = {
+      ...defaultValues,
+      ...formSchema,
+    } as FormSchema;
+
+    const globals = parsedConfig.globals as GlobalsValidationSchema &
+      OutputsValidationSchema;
+
+    if (globals.startPoint && globals.endPoint) {
+      delete globals.steps;
+      globals.timeSpecification = TimeSpecification.DATETIME;
+    } else {
+      globals.timeSpecification = TimeSpecification.STEP;
+    }
+
+    return parsedConfig;
+  }
+
+  private static removeUndefined(obj: any) {
+    const newObj: any = {};
+    Object.keys(obj).forEach((key) => {
+      if (obj[key] === Object(obj[key]))
+        newObj[key] = FormTransformer.removeUndefined(obj[key]);
+      else if (obj[key] !== undefined) newObj[key] = obj[key];
+    });
+    return newObj;
+  }
+
+  private static removeEmptyObj(obj: any) {
+    const newObj: any = {};
+    Object.keys(obj).forEach((key) => {
+      const potentialObj = obj[key];
+
+      if (
+        potentialObj === Object(potentialObj) &&
+        Object.keys(potentialObj).length > 0
+      )
+        newObj[key] = FormTransformer.removeUndefined(potentialObj);
+      else if (
+        potentialObj !== undefined &&
+        Object.keys(potentialObj).length > 0
+      )
+        newObj[key] = potentialObj;
+    });
+    return newObj;
   }
 
   /*
--- src/app/components/Configure/utils/validationSchema.ts
+++ src/app/components/Configure/utils/validationSchema.ts
@@ -1,10 +1,12 @@
 import * as Yup from "yup";
-import { SchemaOf } from "yup";
+import { SchemaOf, TypeOf } from "yup";
 import FieldNames from "./fieldNames";
 import globalsValidationSchema from "../components/globals-form/utils/validationSchema";
 import outputsValidationSchema from "../components/outputs-form/utils/validationSchema";
 
 import { Config } from "./types";
+
+export type ConfigValidationSchema = TypeOf<typeof Schema>;
 
 const Schema: SchemaOf<Config> = Yup.object({
   [FieldNames.GLOBALS]: globalsValidationSchema.concat(outputsValidationSchema),
