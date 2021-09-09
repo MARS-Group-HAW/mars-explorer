aeb05a053ce790659b85f3a29a237609b0575886
Jan-Niklas Voß <jan-niklas.voss@haw-hamburg.de>
2021-09-09 13:59:32 +0200

    WIP: feat(quick-start): add global valid states

---
+++ src/app/components/Configure/utils/config-slice.ts
@@ -0,0 +1,37 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "../../App/utils/store";
+import {
+  initialState as ValidationInitialState,
+  reducers as ValidationReducer,
+  State as ValidationState,
+} from "../../../util/validation-slice";
+
+// Define a type for the slice state
+type State = ValidationState & {
+  path?: string;
+};
+
+// Define the initial state using that type
+const initialState: State = {
+  ...ValidationInitialState,
+};
+
+export const configSlice = createSlice({
+  name: "config",
+  initialState,
+  reducers: {
+    setPath: (state: State, action: PayloadAction<string>) => {
+      state.path = action.payload;
+    },
+    ...ValidationReducer,
+  },
+});
+
+export const { setPath, setUnknownState, setValidState, setInvalidState } =
+  configSlice.actions;
+
+// Other code such as selectors can use the imported `RootState` type
+// TODO: connect to root store
+export const selectConfigPath = (state: RootState) => state;
+
+export default configSlice.reducer;
