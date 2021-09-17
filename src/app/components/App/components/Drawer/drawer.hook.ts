f441f4a727bd30d51482d70ace91594a67ace90f
Jan-Niklas Voß <jan-niklas.voss@haw-hamburg.de>
2021-09-17 11:23:43 +0200

    feat(home): navigate to /Model if the last project was loaded successfully

--- src/app/components/App/components/Drawer/drawer.hook.ts
+++ src/app/components/App/components/Drawer/drawer.hook.ts
@@ -1,5 +1,8 @@
 import { selectProject } from "@app/components/Home/utils/project-slice";
+import { useHistory } from "react-router-dom";
+import { useEffect } from "react";
 import { useAppSelector } from "../../../../utils/hooks/use-store";
+import Path from "../../utils/app-paths";
 
 type State = {
   disableNavigation: boolean;
@@ -7,6 +10,10 @@ type State = {
 
 function useDrawer(): State {
   const { path } = useAppSelector(selectProject);
+
+  const { replace } = useHistory();
+
+  useEffect(() => path && replace(Path.MODEL), [path]);
 
   return {
     disableNavigation: !path,
