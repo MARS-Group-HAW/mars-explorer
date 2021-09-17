4642b84536d4b0f58febafbd1997309c08617dab
Jan-Niklas Voß <jan-niklas.voss@haw-hamburg.de>
2021-09-17 17:03:37 +0200

    feat: save simulation state in global store

--- src/app/utils/store.ts
+++ src/app/utils/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import projectReducer from "../components/Home/utils/project-slice";
 import modelReducer from "../components/Model/utils/model-slice";
+import simulationReducer from "../components/QuickStartBar/utils/simulation-slice";
 
 const store = configureStore({
   reducer: {
     project: projectReducer,
     model: modelReducer,
+    simulation: simulationReducer,
   },
 });
 
