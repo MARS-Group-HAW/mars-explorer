ddfeaee58de60062884c5e77b5ada852fde6d6af
Jan-Niklas Voß <jan-niklas.voss@haw-hamburg.de>
2021-09-17 13:56:39 +0200

    feat(quick-start-bar): add error dialog on simulation error

---
+++ src/electron/handle-simulation-progress.ts
@@ -0,0 +1,82 @@
+import WS from "ws";
+import ReconnectingWebSocket, { Options } from "reconnecting-websocket";
+import { Logger } from "./logger";
+
+export enum WebSocketCloseCodes {
+  RETRYING = 1000,
+  EXITING = 4000,
+}
+
+type ProgressMessage = {
+  currentDateTime: string;
+  currentStep: number;
+  maxTicks: number;
+  currentTick: number;
+  progressInPercentage: number;
+  isTimeReferenced: boolean;
+  oneTickTimeSpan: string;
+  startTimePoint: string;
+  endTimePoint: string;
+  isFinished: boolean;
+  isInitialized: boolean;
+  isPaused: boolean;
+  isAborted: boolean;
+  iterations: number;
+  lastSuccessfullyDateTime: string;
+  lastSuccessfullyStep: number;
+  lastSuccessfullyTick: number;
+};
+
+export function handleSimulationProgress(
+  log: Logger,
+  onProgress: (progress: number) => void,
+  onMaxRetries: () => void
+): () => void {
+  const options: Options = {
+    WebSocket: WS, // custom WebSocket constructor
+    maxRetries: 50,
+    connectionTimeout: 1000,
+  };
+  const rws = new ReconnectingWebSocket(
+    "ws://127.0.0.1:4567/progress",
+    [],
+    options
+  );
+
+  rws.onopen = () => log.info("WebSocket for Simulation opened.");
+
+  let lastProgress: number;
+
+  rws.onmessage = (msg: MessageEvent<string>) => {
+    const { currentTick, maxTicks } = JSON.parse(msg.data) as ProgressMessage;
+    const progress = Math.floor((currentTick / maxTicks) * 100);
+
+    if (lastProgress === undefined || lastProgress < progress) {
+      log.debug(
+        `Simulation-Progress: ${currentTick} von ${maxTicks} (${progress}%)`
+      );
+      onProgress(progress);
+      lastProgress = progress;
+    }
+  };
+  rws.onclose = (msg) => {
+    if (rws.retryCount === options.maxRetries) {
+      log.warn(
+        "Could not connect to the simulation process. Exiting the simulation. Is the 'console' flag set in the config.json?"
+      );
+      onMaxRetries();
+      return;
+    }
+
+    // see codes at https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent/code
+    switch (msg.code) {
+      case WebSocketCloseCodes.RETRYING:
+        log.info("Could not connect to WebSocket. Retrying ...");
+        break;
+      case WebSocketCloseCodes.EXITING:
+        log.info("WebSocket closed by simulation end.");
+        break;
+    }
+  };
+  return () => rws.close(WebSocketCloseCodes.EXITING);
+}
