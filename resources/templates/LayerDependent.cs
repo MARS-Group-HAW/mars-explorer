3118fc3e4ef4103f87ec9be84025cbcc0ea973d3
Jan-Niklas Voß <jan-niklas.voss@haw-hamburg.de>
2021-10-09 11:23:49 +0200

    feat: add dependent layer template

---
+++ resources/templates/LayerDependent.cs
@@ -0,0 +1,10 @@
+using Mars.Components.Layers;
+
+
+namespace $PROJECT_NAME.Model
+{
+    public class $CLASS_NAME : AbstractLayer
+    {
+        public $DEPENDENT_LAYER_NAME DependentLayer { get; set; }
+    }
+}
