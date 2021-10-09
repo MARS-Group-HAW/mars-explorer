86ea742c29adcfa3d529620a1c8293cc74fc1541
Jan-Niklas Voß <jan-niklas.voss@haw-hamburg.de>
2021-10-09 14:30:03 +0200

    feat(model): add different layer creation

---
+++ src/app/components/Model/components/layer-select-button/layer-select-button.tsx
@@ -0,0 +1,123 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import SimObjects from "@shared/types/sim-objects";
+import GridOnIcon from "@material-ui/icons/GridOn";
+
+const options = [
+  SimObjects.BASIC_LAYER,
+  SimObjects.DEPENDENT_LAYER,
+  SimObjects.RASTER_LAYER,
+  SimObjects.VECTOR_LAYER,
+];
+
+type Props = {
+  selected: boolean;
+  onClick: (simObject: SimObjects) => void;
+};
+
+const optionToLabel = (option: string) => option.replace("_", " ");
+
+function LayerSelectButton({ selected, onClick }: Props) {
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef<HTMLDivElement>(null);
+  const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+  const handleClick = () => onClick(options[selectedIndex]);
+
+  const handleMenuItemClick = (
+    event: React.MouseEvent<HTMLLIElement, MouseEvent>,
+    index: number
+  ) => {
+    setSelectedIndex(index);
+    onClick(options[index]);
+    setOpen(false);
+  };
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event: React.MouseEvent<Document, MouseEvent>) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <ButtonGroup
+        color="primary"
+        ref={anchorRef}
+        size="large"
+        variant={selected ? "contained" : "outlined"}
+      >
+        <Button
+          style={{ width: 210 }}
+          onClick={handleClick}
+          startIcon={<GridOnIcon />}
+        >
+          {optionToLabel(options[selectedIndex])}
+        </Button>
+        <Button
+          color="primary"
+          size="small"
+          aria-controls={open ? "split-button-menu" : undefined}
+          aria-expanded={open ? "true" : undefined}
+          aria-label="select merge strategy"
+          aria-haspopup="menu"
+          onClick={handleToggle}
+        >
+          <ArrowDropDownIcon />
+        </Button>
+      </ButtonGroup>
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === "bottom" ? "center top" : "center bottom",
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList id="split-button-menu">
+                  {options.map((option, index) => (
+                    <MenuItem
+                      key={option}
+                      selected={index === selectedIndex}
+                      onClick={(event) => handleMenuItemClick(event, index)}
+                    >
+                      {optionToLabel(option).toLocaleLowerCase()}
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </>
+  );
+}
+
+export default LayerSelectButton;
--- src/shared/types/class-creation-message.ts
+++ src/shared/types/class-creation-message.ts
@@ -1,8 +1,13 @@
 import SimObjects from "./sim-objects";
 
+type DependentLayerArgs = {
+  dependentLayerName: string;
+};
+
 export type ClassCreationMessage = {
   projectPath: string;
   projectName: string;
-  type: SimObjects;
   className: string;
+  type: SimObjects;
+  args?: DependentLayerArgs;
 };
