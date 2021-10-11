f5429c5ef923a867253e21bbd80c0322af92d57f
Jan-Niklas Voß <jan-niklas.voss@haw-hamburg.de>
2021-10-11 16:58:07 +0200

    fix(config): add file input for agent

--- src/app/components/Configure/components/agents-form/agents-form.tsx
+++ src/app/components/Configure/components/agents-form/agents-form.tsx
@@ -1,14 +1,52 @@
 import * as React from "react";
+import { Grid, makeStyles } from "@material-ui/core";
 import useAgentsForm from "./use-agents-form.hook";
 import MappingsForm from "../mappings-form";
-import AgentsEntitiesForm from "../agents-entities-form";
+import FormInput from "../form-input";
+import withNamespace from "../../utils/withNamespace";
+import FieldNames from "../mappings-form/utils/fieldNames";
+import FormFileInput from "../form-file-input";
+
+const useStyles = makeStyles(() => ({
+  nameCountContainer: {
+    height: "20%",
+    padding: 5,
+  },
+}));
 
 const AgentsForm = () => {
+  const classes = useStyles();
+
   const { namespace } = useAgentsForm();
 
   return (
     <MappingsForm>
-      <AgentsEntitiesForm namespace={namespace} />
+      <Grid
+        container
+        justifyContent="space-between"
+        className={classes.nameCountContainer}
+      >
+        <Grid item xs={4}>
+          <FormInput
+            outlined
+            name={withNamespace(FieldNames.NAME, namespace)}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <FormInput
+            outlined
+            name={withNamespace(FieldNames.COUNT, namespace)}
+            type="number"
+            InputProps={{ inputProps: { min: 0 } }}
+          />
+        </Grid>
+        <Grid item xs={5}>
+          <FormFileInput
+            namespace={withNamespace(FieldNames.FILE, namespace)}
+            label={FieldNames.FILE}
+          />
+        </Grid>
+      </Grid>
     </MappingsForm>
   );
 };
--- src/app/components/Configure/components/entities-form/entities-form.tsx
+++ src/app/components/Configure/components/entities-form/entities-form.tsx
@@ -1,14 +1,45 @@
 import * as React from "react";
+import { Grid, makeStyles } from "@material-ui/core";
 import useEntitiesForm from "./use-entities-form.hook";
 import MappingsForm from "../mappings-form";
-import AgentsEntitiesForm from "../agents-entities-form";
+import FormInput from "../form-input";
+import withNamespace from "../../utils/withNamespace";
+import FieldNames from "../mappings-form/utils/fieldNames";
+
+const useStyles = makeStyles(() => ({
+  nameCountContainer: {
+    height: "20%",
+    padding: 5,
+  },
+}));
 
 const EntitiesForm = () => {
+  const classes = useStyles();
+
   const { namespace } = useEntitiesForm();
 
   return (
     <MappingsForm>
-      <AgentsEntitiesForm namespace={namespace} />
+      <Grid
+        container
+        justifyContent="space-between"
+        className={classes.nameCountContainer}
+      >
+        <Grid item>
+          <FormInput
+            outlined
+            name={withNamespace(FieldNames.NAME, namespace)}
+          />
+        </Grid>
+        <Grid item>
+          <FormInput
+            outlined
+            name={withNamespace(FieldNames.COUNT, namespace)}
+            type="number"
+            InputProps={{ inputProps: { min: 0 } }}
+          />
+        </Grid>
+      </Grid>
     </MappingsForm>
   );
 };
