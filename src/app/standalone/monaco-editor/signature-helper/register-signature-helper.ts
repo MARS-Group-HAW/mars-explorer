eb0d3b6fc601cc144ba023806eaf1be034b0b114
Jan-Niklas Voß <jan-niklas.voss@haw-hamburg.de>
2021-10-11 18:42:38 +0200

    refactor: remove unsused snippets

--- src/app/standalone/monaco-editor/signature-helper/register-signature-helper.ts
+++
@@ -1,5 +0,0 @@
-import monaco from "../monaco";
-import { CSHARP } from "../types";
-
-export default (provider: monaco.languages.SignatureHelpProvider) =>
-  monaco.languages.registerSignatureHelpProvider(CSHARP.id, provider);
