39c60c2301846188e25f89616b204d7f9ee2d1e7
Jan-Niklas Voß <jan-niklas.voss@haw-hamburg.de>
2021-10-11 18:42:38 +0200

    refactor: remove unsused snippets

--- src/app/standalone/monaco-editor/snippets/register-snippet.ts
+++
@@ -1,5 +0,0 @@
-import monaco from "../monaco";
-import { CSHARP } from "../types";
-
-export default (provider: monaco.languages.CompletionItemProvider) =>
-  monaco.languages.registerCompletionItemProvider(CSHARP.id, provider);
