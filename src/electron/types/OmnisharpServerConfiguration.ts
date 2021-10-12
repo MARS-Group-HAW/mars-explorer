66d829d495655c6e1382ad770ccd08040be59014
Jan-Niklas Voß <jan-niklas.voss@haw-hamburg.de>
2021-10-12 09:58:52 +0200

    fix(model): code actions not working swapped unmaintained `monaco-languageclient` to `@codingame/monaco-languageclient` and upgraded to v0.16.0

--- src/electron/types/OmnisharpServerConfiguration.ts
+++ src/electron/types/OmnisharpServerConfiguration.ts
@@ -1,18 +1,10 @@
 import { SpawnOptions } from "child_process";
-import { DocumentSelector } from "monaco-languageclient";
-
-export enum SERVER_NAMES {
-  OMNISHARP_TEMP_1349 = "OMNISHARP_TEMP_1349",
-  OMNISHARP_TEMP_13712 = "OMNISHARP_TEMP_13712",
-}
+import { DocumentSelector } from "@codingame/monaco-languageclient";
 
 export type Server = {
   args: string[];
   command: string;
   options: SpawnOptions;
-  // workingDirectory: string;
   language: string;
   documentSelector: DocumentSelector;
 };
-
-export type ServerMap = { [key in keyof typeof SERVER_NAMES]: Server };
