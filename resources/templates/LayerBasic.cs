f9c7034ca6559f705429d88b7b58036f07234c5f
Jan-Niklas Voß <jan-niklas.voss@haw-hamburg.de>
2021-10-19 17:10:36 +0200

    fix: LayerBasic Template

--- resources/templates/LayerBasic.cs
+++ resources/templates/LayerBasic.cs
@@ -1,4 +1,6 @@
 using Mars.Components.Layers;
+using Mars.Interfaces.Data;
+using Mars.Interfaces.Layers;
 
 namespace $PROJECT_NAME
 {
@@ -6,7 +8,7 @@ namespace $PROJECT_NAME
     public class $CLASS_NAME : AbstractLayer
     {
         // Do you want to execute custom logic, initialize Environments or spawn agents?
-        public override bool InitLayer(TInitData layerInitData, RegisterAgent registerAgentHandle, UnregisterAgent unregisterAgentHandle)
+        public override bool InitLayer(LayerInitData layerInitData, RegisterAgent registerAgentHandle, UnregisterAgent unregisterAgentHandle)
         {
             // [IMPORTANT - DO NOT DELETE] do not delete this
             base.InitLayer(layerInitData, registerAgentHandle, unregisterAgentHandle);
