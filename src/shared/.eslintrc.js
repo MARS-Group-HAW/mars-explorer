b412f2e132ff7787b7dcea226c85c16162d85dbd
Jan-Niklas Voß <jan-niklas.voss@haw-hamburg.de>
2021-12-17 14:08:14 +0100

    fix: eslint errors

--- src/shared/.eslintrc.js
+++ src/shared/.eslintrc.js
@@ -1,10 +1,5 @@
 const path = require("path");
 
-const PATH_TO_MAIN_CONFIG = path.join(
-  __dirname,
-  "..",
-  "electron",
-  ".eslintrc.js"
-);
+const PATH_TO_MAIN_CONFIG = path.join(__dirname, "..", "main", ".eslintrc.js");
 
 module.exports = require(PATH_TO_MAIN_CONFIG);
